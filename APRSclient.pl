8b52fc834bbecd995f0e642f335940d1a73f7bbc
phl0 <florian@florian-wolters.de>
2016-01-02 13:40:28 +0100

    Use callsign without SSID for APRS-IS filter

--- APRSclient.pl
+++ APRSclient.pl
@@ -9,6 +9,8 @@ if (! $callsign) {
    printf("Callsign not defined. Please check config file!\n");
    exit 1;
 }
+$filtered_call = $callsign;
+$filtered_call =~ s/-+\d{0,2}//;
 
 $latitude = $cfg->param('Latitude');
 $longitude = $cfg->param('Longitude');
@@ -39,7 +41,7 @@ if (! $passcode) {
 my $session = new Net::Telnet(Timeout => '60');
 $session->errmode('return');
 $session->Net::Telnet::open(Host => $server, Port => $port);
-print $session "user $user pass $passcode vers APRSclient 0.1 filter b/$callsign*\n";
+print $session "user $user pass $passcode vers APRSclient 0.1 filter b/$filtered_call*\n";
 while () {
    $line = $session->getline();
    # Suppres status messages and comments on the net
