5b1ee408b656d1205cee0f226b668f0d944e9769
phl0 <florian@florian-wolters.de>
2016-01-04 09:20:46 +0100

    Skip config error checking for now

--- APRSbouncer.pl
+++ APRSbouncer.pl
@@ -5,43 +5,22 @@ use Net::Telnet;
 $cfg = new Config::Simple('APRSbouncer.conf');
 
 $callsign = $cfg->param('Callsign');
-if (! $callsign) {
-   printf("Callsign not defined. Please check config file!\n");
-   exit 1;
-}
-$filtered_call = $callsign;
-$filtered_call =~ s/-+\d{0,2}//;
-
 $latitude = $cfg->param('Latitude');
 $longitude = $cfg->param('Longitude');
 $comment = $cfg->param('Comment');
 $interval = $cfg->param('Interval');
-
 $server = $cfg->param('Server');
-if (! $server) {
-   printf("Server not defined. Please check config file!\n");
-   exit 2;
-}
 $port = $cfg->param('Port');
-if (! $port) {
-   printf("Port not defined. Please check config file!\n");
-   exit 3;
-}
-$user = $cfg->param('Username');
-if (! $user) {
-   printf("Username not defined. Please check config file!\n");
-   exit 4;
-}
+$username = $cfg->param('Username');
 $passcode = $cfg->param('Passcode');
-if (! $passcode) {
-   printf("Passcode not defined. Please check config file!\n");
-   exit 5;
-}
+
+$filtered_call = $callsign;
+$filtered_call =~ s/-+\d{0,2}//;
 
 my $session = new Net::Telnet(Timeout => '60');
 $session->errmode('return');
 $session->Net::Telnet::open(Host => $server, Port => $port);
-print $session "user $user pass $passcode vers APRSbouncer 0.1 filter b/$filtered_call*\n";
+print $session "user $username pass $passcode vers APRSbouncer 0.1 filter b/$filtered_call*\n";
 while () {
    $line = $session->getline();
    # Suppres status messages and comments on the net
@@ -56,5 +35,5 @@ sub recon {
    print "Error: $msg\n";
    $session->close();
    $session->Net::Telnet::open(Host => $server, Port => $port);
-   print $session "user $user pass $passcode vers APRSbouncer 0.1 filter b/$callsign*\n";
+   print $session "user $username pass $passcode vers APRSbouncer 0.1 filter b/$callsign*\n";
 }
