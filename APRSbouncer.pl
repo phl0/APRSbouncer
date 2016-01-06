90a8a8a50ae2a3d2646c97a34aa33cd7d6c71ec2
phl0 <florian@florian-wolters.de>
2016-01-06 13:26:04 +0100

    Remove double definition of timeout value

--- APRSbouncer.pl
+++ APRSbouncer.pl
@@ -25,7 +25,7 @@ $lat = substr($latitude,2,2).substr($latitude,5,5).substr($latitude,0,1);
 $lon = substr($longitude,2,3).substr($longitude,6,5).substr($longitude,0,1);
 $string = "${callsign}>APB001,TCPIP*:=${lat}${overlay}${lon}${symbol}${comment}";
 
-my $session = new Net::Telnet(Timeout => '60');
+my $session = new Net::Telnet(Timeout => '5');
 $session->errmode('return');
 $session->Net::Telnet::open(Host => $server, Port => $port);
 print $session "user $username pass $passcode vers APRSbouncer 0.1 filter b/$filtered_call*\n";
@@ -33,7 +33,7 @@ sleep 3;
 
 while () {
    # Wait some time for possible beacon on the net
-   if($line = $session->getline(Timeout => 5)) {
+   if($line = $session->getline()) {
       # Suppres status messages and comments on the net
       if ($line =~ /^[^#]/) {
          print $line;
