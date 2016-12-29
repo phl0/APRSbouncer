0bfd1f632a1fec3f55b6f8928257441ebb89a6df
phl0 <florian@florian-wolters.de>
2016-12-29 20:02:16 +0100

    Add basic auto answer functionality

--- APRSbouncer.pl
+++ APRSbouncer.pl
@@ -19,42 +19,59 @@ $server = $cfg->param('Server');
 $port = $cfg->param('Port');
 $username = $cfg->param('Username');
 $passcode = $cfg->param('Passcode');
-$version = "1.0";
+$autoanswer = $cfg->param('AutoAnswer');
+$version = "1.1";
+$aprsdst = $version =~ s/\.//r;
+$aprsdst = sprintf "%03d", $aprsdst;
+$aprsdst = "APB".$aprsdst;
 $debug = 0;
 
 $lat = substr($latitude,2,2).substr($latitude,5,5).substr($latitude,0,1);
 $lon = substr($longitude,2,3).substr($longitude,6,5).substr($longitude,0,1);
-$beaconstring = "${callsign}>APB001,TCPIP*:=${lat}${overlay}${lon}${symbol}${comment}";
+$beaconstring = "${callsign}>$aprsdst,TCPIP*:=${lat}${overlay}${lon}${symbol}${comment}";
 
 my $session = new Net::Telnet(Timeout => '30');
 $session->errmode('return');
 $session->Net::Telnet::open(Host => $server, Port => $port);
-print $session "user $username pass $passcode vers APRSbouncer $version filter b/$callsign*\n";
+print $session "user $username pass $passcode vers APRSbouncer $version filter b/$callsign* g/$callsign*\n";
 sleep 1;
 my $lastbeaconin = time();
 my $lastbeaconsent = time();
 print $session "$beaconstring\n";
-print "\rLast beacon on APRS-IS: ";
+print "Last beacon on APRS-IS: ";
 print strftime("%H:%M:%S ", localtime($lastbeaconin));
 print "; last own beacon sent: ";
 print strftime("%H:%M:%S", localtime($lastbeaconsent));
+print "\n";
+my $messagecount = 0;
 
 while () {
    # Wait some time for possible beacon on the net
    if($line = $session->getline()) {
       # If we receive a beacon with own callsign on the net just 
       # reset the lastbeaconin timer
-      if ($line =~ /^$callsign>/) {
-         $lastbeaconin = time();
-         print "\rLast beacon on APRS-IS: ";
-         print strftime("%H:%M:%S ", localtime($lastbeaconin));
-         print "; last own beacon sent: ";
-         print strftime("%H:%M:%S", localtime($lastbeaconsent));
+      if ($line =~ m/^$callsign>/) {
+         # If we receive an ACK from another instance we do not need 
+         # to answer but instead reset the message counter
+         if ($line =~ m/ack\d+$/) {
+            $messagecount = 0;
+         } else {
+            $lastbeaconin = time();
+            print "Last beacon on APRS-IS: ";
+            print strftime("%H:%M:%S ", localtime($lastbeaconin));
+            print "; last own beacon sent: ";
+            print strftime("%H:%M:%S", localtime($lastbeaconsent));
+            print "\n";
+         }
+         if ($debug) {
+            print strftime("%H:%M:%S ", localtime(time()));
+            print $line;
+         }
       }
       # Suppres status messages and comments on the net
       # but also handle own beacon as that is only executed if getline 
       # succeeded
-      if ($line =~ /^#/) {
+      elsif ($line =~ /^#/) {
          if ($debug) {
             print strftime("%H:%M:%S ", localtime(time()));
             print $line;
@@ -63,10 +80,42 @@ while () {
          if ((time() - $lastbeaconin) > ($interval * 60 + 60) && (time() - $lastbeaconsent) > ($interval * 60)) {
             print $session "$beaconstring\n";
             $lastbeaconsent = time();
-            print "\rLast beacon on APRS-IS: ";
+            print "Last beacon on APRS-IS: ";
             print strftime("%H:%M:%S ", localtime($lastbeaconin));
             print "; last own beacon sent: ";
             print strftime("%H:%M:%S", localtime($lastbeaconsent));
+            print "\n";
+         }
+      }
+      elsif ($line =~ m/::$callsign\s*:/) {
+         if ($debug) {
+            print strftime("%H:%M:%S ", localtime(time()));
+            print $line."\n";
+         }
+         $messagecount++;
+         if ($messagecount > 1) {
+            if ($line =~ m/^(\w*)>([a-zA-Z0-9,*-]+)::(\w+)\s*:(.*){(.*)$/) {
+               my $from = $1;
+               my $path = $2;
+               my $to = $3;
+               my $message = $4;
+               my $ackno = $5;
+               $from = sprintf "%-9s", $from;
+               my $answer = "$callsign>$aprsdst,TCPIP*::$from:ack$ackno";
+               print $session "$answer\n";
+               my $ack = int(rand(100));
+               my $autoanswer = "$callsign>$aprsdst,TCPIP*::$from:${autoanswer}\{$ack";
+               print $session "$autoanswer\n";
+               print strftime("%H:%M:%S ", localtime(time()));
+               print "Sent auto answer to $from\n";
+               $messagecount = 0;
+            }
+         }
+      }
+      else {
+         if ($debug) {
+            print strftime("%H:%M:%S ", localtime(time()));
+            print $line;
          }
       }
    }
@@ -83,5 +132,5 @@ sub recon {
    $session = new Net::Telnet(Timeout => '30');
    $session->errmode('return');
    $session->Net::Telnet::open(Host => $server, Port => $port);
-   print $session "user $username pass $passcode vers APRSbouncer $version filter b/$callsign*\n";
+   print $session "user $username pass $passcode vers APRSbouncer $version filter b/$callsign* g/$callsign*\n";
 }
